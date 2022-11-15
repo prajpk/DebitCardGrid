3c62d1ed404cd8f1384c23fad63bb0c3c35ba98c
prajpk <prajpk@DESKTOP-91S154T>
2022-11-15 07:25:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿int rowCount = 5;
+int columnCount = 26;
+List<int> gridNums;
+string grid;
+int randNum = 0;
+
+for (int r = 0; r < rowCount; r++)
+{
+    grid = "";
+    gridNums = new List<int>();
+    for (int c = 0; c < columnCount; c++)
+    {
+    again:
+        randNum = new Random().Next(1, 99);
+        if (!gridNums.Contains(randNum))
+        {
+            gridNums.Add(randNum);
+        }
+        else
+        {
+            goto again;
+        }
+        if (c < columnCount - 1)
+        {
+            grid += randNum + ", ";
+        }
+        else
+        {
+            grid += randNum;
+        }
+    }
+    Console.WriteLine(grid);
+}
